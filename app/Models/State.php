7d40fa685636856cac0506ac757e69726faa8ecc
Padilha1 <padilha.matheus@hotmail.com>
2025-09-30 14:34:16 -0300

    Filament Resources - CRUD

--- app/Models/State.php
+++ app/Models/State.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    protected $fillable =['country_id', 'name'];
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+    public function employees(){
+        return $this->hasMany(Employee::class);
+    }
 }
